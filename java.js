c989e18843798f81ea476df1165c8f03417a708e
Emmanuel Temprano <tempra.8994@gmail.com>
2022-07-10 20:30:23 -0300

    Eventos

---
+++ java.js
@@ -0,0 +1,15 @@
+let botonUno = document.getElementById("btnEvento1")
+let botonDos = document.getElementById("btnEvento2")
+let botonTres = document.getElementById("btnEvento3")
+
+botonUno.addEventListener("click", () => {
+    alert("Juan Ricardo Estevez")
+} )
+
+botonDos.onclick = () =>{
+    alert("Juan.r.e@gmail.com")
+}
+
+botonTres.addEventListener("click", () => {
+    alert("Saavedra 89 - Departamento 8-C")
+} )
